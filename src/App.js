d3ce015ccc235a9a92e8f2fd15c710277b515a8a
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-06-17 18:58:02 +0530

    createPortal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./Components/Meals/Meals";
+import Cart from './Components/Cart/Cart';
 
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
        <main>
         <Meals />
