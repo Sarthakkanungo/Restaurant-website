2df822521321be2f673082d5a5149d037ce3fb13
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-06-17 16:28:53 +0530

    Meals Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./Components/Layout/Header";
 import { Fragment } from "react";
+import Meals from "./Components/Meals/Meals";
 
 
 function App() {
   return (
     <Fragment>
       <Header />
+       <main>
+        <Meals />
+       </main>
     </Fragment>
   );
 }
