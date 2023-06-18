7a83022634e030755333050dc33753236871ad5e
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-06-18 10:26:58 +0530

    Cart Closing on button click

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,13 +3,17 @@ import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 import HeaderCardButton from "./HeaderCardButton";
 
+
 const Header = (props) => {
+
+ 
   return (
     <Fragment>
        <header className={classes.header}>
         <h1> React Meals </h1>
-        <HeaderCardButton />
+        <HeaderCardButton onClick={props.onShowCart} />
        </header>
+      
        <div className={classes["main-image"]}>
           <img src={mealsImage} alt="A table full of delicious food!"/>
        </div>
--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,22 +1,16 @@
 import CartIcon from "../Cart/Carticon";
-import classes from './HeaderCardButton.module.css';
+import classes from "./HeaderCardButton.module.css";
 
-const HeaderCardButton = props => {
-    return (
-        <button className={classes.button}>
-            <span className={classes.icon}>
-                <CartIcon />
-            </span>
-            <span>
-                Your Cart
-            </span>
-            <span className={classes.badge}>
-                3
-            </span>
-
-
-        </button>
-    )
-
-}
+const HeaderCardButton = (props) => {
+  return (
+    <button className={classes.button}  onClick={props.onClick}>
+        
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
 export default HeaderCardButton;
