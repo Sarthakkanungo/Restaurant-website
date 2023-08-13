8b307bb575687d73204e8ef687a88a49447ae87f
sarthakkanungo <sarthak.kanungo001@gmail.com>
2023-08-13 16:45:06 +0530

    dollar to rupee

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -9,7 +9,7 @@ import CartContext from '../../../store/cart-context';
 const MealItem =(props) => {
 
  const cartCtx = useContext(CartContext);
-  const price = `₹${props.price.toFixed(2)}` ;
+  const price = `$${props.price.toFixed(2)}` ;
 
   const addToCartHandler = (amount) => {
 
